d91ef5a5917cb9bd5487b1c228725a6e3d755dbf
Lucas-Pedreira <lucasrpedreira@gmail.com>
2022-08-29 14:14:21 -0300

    ex1feito

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+// let nome
+// let idade
+// console.log (typeof nome, typeof idade)
+//foi impresso 'undefined' porque as variaveis não tem valor.
+ let nome = prompt ("Qual é o seu nome ?")
+ let idade = prompt ("Qual é a sua idade ?")
+ //console.log (nome, typeof nome)
+ //console.log (idade, typeof idade)
+ //as duas são string
+ console.log (`Olá, ${nome} você tem ${idade} anos.`)
